4c3bc846e5492d33570f50a4f6dea1dfc9e0333f
anilyildiz <anil-yildiz@outlook.com.tr>
2024-06-03 20:02:29 +0300

    Name properties added and used in end point

--- src/main/java/com/example/springdemo/springdemo/rest/FirstRestController.java
+++ src/main/java/com/example/springdemo/springdemo/rest/FirstRestController.java
@@ -1,10 +1,17 @@
 package com.example.springdemo.springdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstRestController {
+
+    @Value("${brother.name}")
+    private String brotherName;
+
+    @Value("${sister.name}")
+    private String sisterName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -19,6 +26,11 @@ public class FirstRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is you lucky day";
+    }
+
+    @GetMapping("/familyInfo")
+    public String familyInfo() {
+        return brotherName + " and " + sisterName;
     }
 
 }
