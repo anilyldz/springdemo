f70e0b81dd90d92d7b8e6a5a9771a208ea37d3e6
anilyildiz <anil-yildiz@outlook.com.tr>
2024-06-06 20:56:44 +0300

    Lifecycle Methods example

--- src/main/java/com/example/springdemo/springdemo/domain/BasketballCoach.java
+++ src/main/java/com/example/springdemo/springdemo/domain/BasketballCoach.java
@@ -1,5 +1,7 @@
 package com.example.springdemo.springdemo.domain;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -9,8 +11,18 @@ import org.springframework.stereotype.Component;
 //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BasketballCoach implements Coach{
 
+    @PostConstruct
+    public void doStartupStuff(){
+        System.out.println("Doing start up stuff");
+    }
+
     public BasketballCoach() {
         System.out.println("In constructor " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doPreDestroyStuff(){
+        System.out.println("Doing pre destroy stuff");
     }
 
     @Override
